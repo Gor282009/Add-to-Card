cabf6c01836d53630f83fc6434880b2eff848b86
Gor282009 <vardanyangor46@gmail.com>
2024-09-07 12:02:06 +0400

    Add to CArd

---
+++ src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../App.css"
+
+function Card() {
+  return (
+    <div className='cont'>
+        <h1>This is a Card Component</h1>
+        <button className="remove-btn">Remove</button>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/Components/store.jsx
@@ -0,0 +1,9 @@
+import { createStore } from "redux";
+
+const store = createStore(
+  function (state, action) {
+    
+  }
+);
+
+export default store;
